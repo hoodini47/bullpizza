58678297a455204e01b7bf0559c2b91380bdc71d
hoodini47 <msajdera@gmail.com>
2020-11-23 18:33:18 +0100

    FadeIn effect on Main Page removed.

--- src/components/section-welcome.js
+++ src/components/section-welcome.js
@@ -1,46 +1,47 @@
-import React from 'react'
+import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 import "../styles/section-welcome.scss"
 import BbgSvgLogo from "../images/bullpizza-logo-large.inline.svg"
 import ArrowLogo from "../images/arrow.inline.svg"
-import scrollTo from 'gatsby-plugin-smoothscroll'
-import Zoom from 'react-reveal/Zoom';
-
+import scrollTo from "gatsby-plugin-smoothscroll"
+import Zoom from "react-reveal/Zoom"
 
 export const SectionWelcome = () => {
-
-  const data =  useStaticQuery(graphql`
-  query MyMainPhotoQuery {
-    datoCmsMainPhoto {
-      mainPhoto {
-        fluid {
-          ...GatsbyDatoCmsFluid
+  const data = useStaticQuery(graphql`
+    query MyMainPhotoQuery {
+      datoCmsMainPhoto {
+        mainPhoto {
+          fluid {
+            src
+          }
         }
       }
     }
-  }
-  
-`)
+  `)
 
- return (
-  <section id={`welcome`} className={`first-section`}>
-    <Img fadeIn fluid={data.datoCmsMainPhoto.mainPhoto.fluid}/>
+  return (
+    <section id={`welcome`} className={`first-section`}>
+      <Img fluid={data.datoCmsMainPhoto.mainPhoto.fluid} />
 
-    <Zoom>
-      <div className={`bullpizza-logo`}>
-        <BbgSvgLogo />
+      <Zoom>
+        <div className={`bullpizza-logo`}>
+          <BbgSvgLogo />
+        </div>
+      </Zoom>
+
+      <div
+        role="button"
+        tabIndex={0}
+        className={`scroll-icon`}
+        onClick={() => scrollTo("#menu")}
+      >
+        <span className={`arrow-container`}>
+          <ArrowLogo />
+        </span>
       </div>
-    </Zoom>
-    
-    <div role="button" tabIndex={0} className={`scroll-icon`} onClick={() => scrollTo('#menu')}>
-      <span className={`arrow-container`}><ArrowLogo /></span>
-    </div>
-  </section>
- )
+    </section>
+  )
 }
 
 export default SectionWelcome
-
-
-
